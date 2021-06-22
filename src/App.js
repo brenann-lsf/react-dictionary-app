2a0015ddf6bba823b065f56833449c5e91b6d3f8
brenann-lsf <brenannfrancis@gmail.com>
2021-06-21 20:57:54 -0400

    Added Dictionary Component and Search Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <div className="container">
+        <header>Dictionary</header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer><small>Coded by Brenann Francis</small></footer>
+      </div>
     </div>
   );
 }
